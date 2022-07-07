5be5bb8975e53252704f14d73f79c670869c1234
Adhimalu <adhimalu678@gmail.com>
2022-07-07 22:22:46 +0530

    view_course updated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AddcourseComponent } from './addcourse/addcourse.component';
+import { ViewBooksComponent } from './view-books/view-books.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AddcourseComponent
+    AddcourseComponent,
+    ViewBooksComponent
   ],
   imports: [
     BrowserModule,
